c7976fa17708ef11a979c2fcdd20e11224ab1213
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-03-10 12:43:49 -0300

    aula 8

---
+++ projeto1/aula8.py
@@ -0,0 +1,21 @@
+nome = 'Rafael'
+sobrenome = 'Ribeiro'
+nome_completo = nome + ' ' + sobrenome
+idade = 27
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura = 1.83
+print('Nome:', nome, end=' ')
+print(type(nome))
+print('Sobrenome:', sobrenome, end=' ')
+print(type(sobrenome))
+print('Nome completo:', nome_completo, end=' ')
+print(type(nome_completo))
+print('Idade:', idade, end=' ')
+print(type(idade))
+print('Ano de nascimento:', ano_nascimento, end=' ')
+print(type(ano_nascimento))
+print('É maior de idade?', maior_de_idade, end=' ')
+print(type(maior_de_idade))
+print('Altura em metros:', altura, end=' ')
+print(type(altura))
