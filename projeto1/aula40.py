4fe864d7d95ada438f7dccd2856af5a2fc4be2fa
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-03-16 12:07:10 -0300

    exercicio aula 40 calculadora

--- projeto1/aula40.py
+++ projeto1/aula40.py
@@ -1,10 +1,12 @@
 """ Calculadora com while """
 while True:
     numero_1 = input('Digite um número: ')
-    numero_2 = input('Digite outro número: ')
     operador = input('Digite o operador (+-/*): ')
+    numero_2 = input('Digite outro número: ')
 
     numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
 
     try:
         num_1_float = float(numero_1)
@@ -26,6 +28,23 @@ while True:
     if len(operador) > 1:
         print('Digite apenas um operador.')
         continue
+    
+    print('Calculando...')
+    if operador == '+':
+        resultado = num_1_float + num_2_float
+    elif operador == '-':
+        resultado = num_1_float - num_2_float
+    elif operador == '*':
+        resultado = num_1_float * num_2_float
+    elif operador == '/':
+        resultado = num_1_float / num_2_float
+    else:
+        print('Operador inválido.')
+        continue
+
+    print(f'{numero_1} {operador} {numero_2} = {resultado}')
+    
+
 
     ###
 
